25150ce1a9b92164afaf7e4df421ec0027043f6d
Dmytro Ivashchuk <dimaiv@me.com>
2019-08-03 17:38:11 +0200

    allow for retrieving info of users passed from cron to queue

--- rabbit/consumer-1.js
+++ rabbit/consumer-1.js
@@ -1,19 +1,38 @@
+require("dotenv").config();
+const twitterMethods = require("../twitter/twitter");
+
 const q = "task";
 
-const open = require("amqplib").connect("amqp://guest:guest@localhost");
+const open = require("amqplib").connect(process.env.AMPQ_CONNECTION_STRING);
 
 open
-    .then(function (conn) {
-        return conn.createChannel();
-    })
-    .then(function (ch) {
-        return ch.assertQueue(q).then(function (ok) {
-            return ch.consume(q, function (msg) {
-                if (msg !== null) {
-                    console.log(msg.content.toString());
-                    ch.ack(msg);
-                }
-            });
-        });
-    })
-    .catch(console.warn);
+  .then(function(conn) {
+    return conn.createChannel();
+  })
+  .then(function(ch) {
+    return ch.assertQueue(q).then(function(ok) {
+      return ch.consume(q, async function(msg) {
+        if (msg !== null) {
+          const twitterInfo = await twitterMethods.getUserInfo(
+            msg.content.toString()
+          );
+          const {
+            followers_count,
+            friends_count,
+            listed_count,
+            favourites_count
+          } = twitterInfo;
+
+          console.log({
+            followers_count,
+            friends_count,
+            listed_count,
+            favourites_count,
+            timestamp: Math.floor(Date.now() / 1000)
+          });
+          ch.ack(msg);
+        }
+      });
+    });
+  })
+  .catch(console.warn);
